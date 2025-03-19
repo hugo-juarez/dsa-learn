2bbf82f4d13c9a78a11492fd95ef312e2b0383f9
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-03-18 21:17:17 -0500

    initial dynamic array in c

---
+++ array-adt/array-adt/main.h
@@ -0,0 +1,17 @@
+//
+//  main.h
+//  array-adt
+//
+//  Created by Hugo Juarez on 3/18/25.
+//
+
+#ifndef main_h
+#define main_h
+
+typedef struct {
+    int *A;
+    int size;
+    int lenght;
+} Array;
+
+#endif /* main_h */
