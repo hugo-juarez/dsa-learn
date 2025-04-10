72744b8efeb6faf5fc53c740e38717030da2578a
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-04-09 21:11:04 -0500

    initialize binary search tree

---
+++ binary-search-tree/binary-search-tree/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  binary-search-tree
+//
+//  Created by Hugo Juarez on 4/9/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
