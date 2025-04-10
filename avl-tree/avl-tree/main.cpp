123cd56b9160469b35d7ad7274dd240deb4ceae5
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-04-10 18:32:40 -0500

    initialize avl tree project

---
+++ avl-tree/avl-tree/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  avl-tree
+//
+//  Created by Hugo Juarez on 4/10/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
