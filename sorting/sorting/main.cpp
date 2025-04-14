62b6fd2a6c39d57fac957afd7144c4aa4065a710
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-04-14 18:06:08 -0500

    initialize sorting project

---
+++ sorting/sorting/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sorting
+//
+//  Created by Hugo Juarez on 4/14/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
