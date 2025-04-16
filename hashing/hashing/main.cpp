0b369c0f2ab2262e8de1dc3d8b813abf5a3f92fd
Hugo Juarez <hsebastianjuarezp@gmail.com>
2025-04-15 19:06:41 -0500

    initialize hashing project

---
+++ hashing/hashing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hashing
+//
+//  Created by Hugo Juarez on 4/15/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
